30a302c7a967618f617800f36a4ff2030a685fef
wfparrish <williparrish@gmail.com>
2021-02-20 18:39:51 -0700

    Set up the two values that must be looped and their division results used to populate a new ArrayList that will be used to populate the chart

--- app/src/main/java/com/example/mpchart01/MainActivity.java
+++ app/src/main/java/com/example/mpchart01/MainActivity.java
@@ -32,28 +32,27 @@ public class MainActivity extends AppCompatActivity {
     LineChart mpLineChart;
     Button bitCoinApiButton;
     private double[] bitCoinData;
+    private JSONObject[] cornData;
     ArrayList<Entry> dataVals;
+    ArrayList<Entry> cornVals;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        String URL="https://api.coindesk.com/v1/bpi/historical/close.json";
+        String URL1="https://api.coindesk.com/v1/bpi/historical/close.json";
+        String URL2="https://api.marketstack.com/v1/eod?access_key=86746e122918145ebf3f1473bf378d26&symbols=CORN";
 
         RequestQueue requestQueue = Volley.newRequestQueue(this);
 
-        //So I need multiple objectRequest instance
-        //ex. objectRequest1, objectRequest2
-        //object
-
-        JsonObjectRequest objectRequest = new JsonObjectRequest(Request.Method.GET,
-                URL,
+        JsonObjectRequest objectRequest1 = new JsonObjectRequest(Request.Method.GET,
+                URL1,
                 null,
                 new Response.Listener<JSONObject>() {
                     @Override
                     public void onResponse(JSONObject response) {
-                        //Log.e("Rest Response", response.toString());
+                        //Log.e("Bitcoin Response", response.toString());
                         dataVals = bitcoinDataFunction(response);
                     }
                 },
@@ -65,7 +64,26 @@ public class MainActivity extends AppCompatActivity {
                 }
         );
 
-        requestQueue.add(objectRequest);
+        JsonObjectRequest objectRequest2 = new JsonObjectRequest(Request.Method.GET,
+                URL2,
+                null,
+                new Response.Listener<JSONObject>() {
+                    @Override
+                    public void onResponse(JSONObject response) {
+                        //Log.e("Corn stock Response", response.toString());
+                      cornVals =  cornDataFunction(response);
+                    }
+                },
+                new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        Log.e("Rest Response", error.toString());
+                    }
+                }
+        );
+
+        requestQueue.add(objectRequest1);
+        requestQueue.add(objectRequest2);
 
         //Links the line chart XML to the java code
         mpLineChart = (LineChart) findViewById(R.id.line_chart);
@@ -75,6 +93,9 @@ public class MainActivity extends AppCompatActivity {
         bitCoinApiButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+
+                System.out.println(dataVals.get(0).getY());
+                System.out.println(cornVals.get(0).getY());
 
                 //Inside here is where we are going to divide bitcoins by corn share prices
                 //Eventually passing the values to an arraylist named dataVals, which will
@@ -93,12 +114,11 @@ public class MainActivity extends AppCompatActivity {
     }//end of onCreate()
 
       private ArrayList<Entry> bitcoinDataFunction(JSONObject response) {
-        System.out.println("Bitcoin API data starting here: ");
-        System.out.println(response.toString());
+        //System.out.println("Bitcoin API data starting here: ");
+        //System.out.println(response.toString());
           ArrayList<Entry> dataVals = new ArrayList<>();
           try {
               bitCoinData = new double[] {
-                      (double) response.getJSONObject("bpi").get("2021-01-20"),
                       (double) response.getJSONObject("bpi").get("2021-01-21"),
                       (double) response.getJSONObject("bpi").get("2021-01-22"),
                       (double) response.getJSONObject("bpi").get("2021-01-23"),
@@ -129,6 +149,7 @@ public class MainActivity extends AppCompatActivity {
                       (double) response.getJSONObject("bpi").get("2021-02-17"),
                       (double) response.getJSONObject("bpi").get("2021-02-18"),
                       (double) response.getJSONObject("bpi").get("2021-02-19"),
+
               };
 
               dataVals.add(new Entry(0, (float) bitCoinData[0]));
@@ -161,7 +182,6 @@ public class MainActivity extends AppCompatActivity {
               dataVals.add(new Entry(27, (float) bitCoinData[27]));
               dataVals.add(new Entry(28, (float) bitCoinData[28]));
               dataVals.add(new Entry(29, (float) bitCoinData[29]));
-              dataVals.add(new Entry(30, (float) bitCoinData[30]));
 
           } catch (JSONException e) {
               e.printStackTrace();
@@ -170,57 +190,112 @@ public class MainActivity extends AppCompatActivity {
         return dataVals;
       }
 
-//    private ArrayList<Entry> dataValues1() {
-//        ArrayList<Entry> dataVals = new ArrayList<Entry>();
-//        dataVals.add(new Entry(0,5));
-//        dataVals.add(new Entry(500,7));
-//        dataVals.add(new Entry(1100,24));
-//        dataVals.add(new Entry(6000,22));
-//        dataVals.add(new Entry(11200,4));
-//        dataVals.add(new Entry(15200,6));
-//        dataVals.add(new Entry(21300,10));
-//        dataVals.add(new Entry(24300,12));
-//        dataVals.add(new Entry(30400,24));
-//        dataVals.add(new Entry(40400,29));
-//        dataVals.add(new Entry(51000,12));
-//        dataVals.add(new Entry(52000,27));
-//        dataVals.add(new Entry(55000,7));
-//        dataVals.add(new Entry(58000,5));
-//        dataVals.add(new Entry(63000,11));
-//        dataVals.add(new Entry(69000,10));
-//        dataVals.add(new Entry(74000,32));
-//        dataVals.add(new Entry(87000,11));
-//        dataVals.add(new Entry(96000,19));
-//        dataVals.add(new Entry(102000,13));
-//        dataVals.add(new Entry(110000,24));
-//        dataVals.add(new Entry(115000,15));
-//        dataVals.add(new Entry(120000,21));
-//        dataVals.add(new Entry(125000,12));
-//        dataVals.add(new Entry(130000,11));
-//        dataVals.add(new Entry(135000,17));
-//        dataVals.add(new Entry(140000,25));
-//        dataVals.add(new Entry(145000,5));
-//        dataVals.add(new Entry(150000,19));
-//        dataVals.add(new Entry(155000,20));
-//        dataVals.add(new Entry(166600,13));
-//        dataVals.add(new Entry(175600,24));
-//        dataVals.add(new Entry(177700,27));
-//        dataVals.add(new Entry(180000,25));
-//        dataVals.add(new Entry(188800,35));
-//        dataVals.add(new Entry(200000,7));
-//        dataVals.add(new Entry(230000,28));
-//        dataVals.add(new Entry(275000,29));
-//        dataVals.add(new Entry(300000,7));
-//        dataVals.add(new Entry(315000,4));
-//        dataVals.add(new Entry(325000,3));
-//        dataVals.add(new Entry(335000,17));
-//        dataVals.add(new Entry(350000,9));
-//        dataVals.add(new Entry(362000,12));
-//        dataVals.add(new Entry(375000,11));
-//        dataVals.add(new Entry(385000,17));
-//        dataVals.add(new Entry(394000,28));
-//        dataVals.add(new Entry(400000,29));
-//        return dataVals;
-//    }//end of dataValues1()
+    private ArrayList<Entry> cornDataFunction(JSONObject response) {
+        //System.out.println("Corn API data starting here: ");
+        //System.out.println(response.toString());
+        ArrayList<Entry> cornVals = new ArrayList<>();
+        try {
+            cornData = new JSONObject[] {
+                     response.getJSONArray("data").getJSONObject(0),
+                     response.getJSONArray("data").getJSONObject(1),
+                     response.getJSONArray("data").getJSONObject(2),
+                     response.getJSONArray("data").getJSONObject(3),
+                     response.getJSONArray("data").getJSONObject(4),
+                     response.getJSONArray("data").getJSONObject(5),
+                     response.getJSONArray("data").getJSONObject(6),
+                     response.getJSONArray("data").getJSONObject(7),
+                     response.getJSONArray("data").getJSONObject(8),
+                     response.getJSONArray("data").getJSONObject(9),
+                     response.getJSONArray("data").getJSONObject(10),
+                     response.getJSONArray("data").getJSONObject(11),
+                     response.getJSONArray("data").getJSONObject(12),
+                     response.getJSONArray("data").getJSONObject(13),
+                     response.getJSONArray("data").getJSONObject(14),
+                     response.getJSONArray("data").getJSONObject(15),
+                     response.getJSONArray("data").getJSONObject(16),
+                     response.getJSONArray("data").getJSONObject(17),
+                     response.getJSONArray("data").getJSONObject(18),
+                     response.getJSONArray("data").getJSONObject(19),
+                     response.getJSONArray("data").getJSONObject(20),
+                     response.getJSONArray("data").getJSONObject(21),
+                     response.getJSONArray("data").getJSONObject(22),
+                     response.getJSONArray("data").getJSONObject(23),
+                     response.getJSONArray("data").getJSONObject(24),
+                     response.getJSONArray("data").getJSONObject(25),
+                     response.getJSONArray("data").getJSONObject(26),
+                     response.getJSONArray("data").getJSONObject(27),
+                     response.getJSONArray("data").getJSONObject(28),
+                     response.getJSONArray("data").getJSONObject(29),
+            };
+
+            double coData0 = cornData[0].getDouble("open");
+            double coData1 = cornData[1].getDouble("open");
+            double coData2 = cornData[2].getDouble("open");
+            double coData3 = cornData[3].getDouble("open");
+            double coData4 = cornData[4].getDouble("open");
+            double coData5 = cornData[5].getDouble("open");
+            double coData6 = cornData[6].getDouble("open");
+            double coData7 = cornData[7].getDouble("open");
+            double coData8 = cornData[8].getDouble("open");
+            double coData9 = cornData[9].getDouble("open");
+            double coData10 = cornData[10].getDouble("open");
+            double coData11 = cornData[11].getDouble("open");
+            double coData12 = cornData[12].getDouble("open");
+            double coData13 = cornData[13].getDouble("open");
+            double coData14 = cornData[14].getDouble("open");
+            double coData15 = cornData[15].getDouble("open");
+            double coData16 = cornData[16].getDouble("open");
+            double coData17 = cornData[17].getDouble("open");
+            double coData18 = cornData[18].getDouble("open");
+            double coData19 = cornData[19].getDouble("open");
+            double coData20 = cornData[20].getDouble("open");
+            double coData21 = cornData[21].getDouble("open");
+            double coData22 = cornData[22].getDouble("open");
+            double coData23 = cornData[23].getDouble("open");
+            double coData24 = cornData[24].getDouble("open");
+            double coData25 = cornData[25].getDouble("open");
+            double coData26 = cornData[26].getDouble("open");
+            double coData27 = cornData[27].getDouble("open");
+            double coData28 = cornData[28].getDouble("open");
+            double coData29 = cornData[29].getDouble("open");
+
+
+            cornVals.add(new Entry(0, (float) coData0));
+            cornVals.add(new Entry(1, (float) coData1));
+            cornVals.add(new Entry(2, (float) coData2));
+            cornVals.add(new Entry(3, (float) coData3));
+            cornVals.add(new Entry(4, (float) coData4));
+            cornVals.add(new Entry(5, (float) coData5));
+            cornVals.add(new Entry(6, (float) coData6));
+            cornVals.add(new Entry(7, (float) coData7));
+            cornVals.add(new Entry(8, (float) coData8));
+            cornVals.add(new Entry(9, (float) coData9));
+            cornVals.add(new Entry(10, (float) coData10));
+            cornVals.add(new Entry(11, (float) coData11));
+            cornVals.add(new Entry(12, (float) coData12));
+            cornVals.add(new Entry(13, (float) coData13));
+            cornVals.add(new Entry(14, (float) coData14));
+            cornVals.add(new Entry(15, (float) coData15));
+            cornVals.add(new Entry(16, (float) coData16));
+            cornVals.add(new Entry(17, (float) coData17));
+            cornVals.add(new Entry(18, (float) coData18));
+            cornVals.add(new Entry(19, (float) coData19));
+            cornVals.add(new Entry(20, (float) coData20));
+            cornVals.add(new Entry(21, (float) coData21));
+            cornVals.add(new Entry(22, (float) coData22));
+            cornVals.add(new Entry(23, (float) coData23));
+            cornVals.add(new Entry(24, (float) coData24));
+            cornVals.add(new Entry(25, (float) coData25));
+            cornVals.add(new Entry(26, (float) coData26));
+            cornVals.add(new Entry(27, (float) coData27));
+            cornVals.add(new Entry(28, (float) coData28));
+            cornVals.add(new Entry(29, (float) coData29));
+
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+
+        return cornVals;
+    }
 
 }//end of MainActivity
